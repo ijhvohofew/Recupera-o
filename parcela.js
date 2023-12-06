4d98946e6f619c789cce05451c4f3b4929fb5424
ijhvohofew <152894872+ijhvohofew@users.noreply.github.com>
2023-12-06 12:39:15 -0300

    Add files via upload

---
+++ parcela.js
@@ -0,0 +1,19 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("pre")
+
+frm.addEventListener("submit", (e)=> {
+ e.preventDefault()
+
+ const valor = Number(frm.inValor.value)
+ const num = Number(frm.inParcela.value)
+ const valorParcela = Math.floor(valor /num)
+ let valorFinal= valorParcela + (valor % num)
+ let resposta = ""
+
+ for(let i = 1; i < num; i++){
+   resposta = resposta + i + "ª Parcelas: R$:" + valorParcela.toFixed(2) + "\n"
+}
+
+resposta = resposta + num + "ª Parcelas: R$:" + valorFinal.toFixed(2)
+ resp.innerText = resposta
+})
